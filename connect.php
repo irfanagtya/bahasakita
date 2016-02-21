56635819ba87d6ba9b4daf421a9f2a10b48d151a
elneunizar <elneunizar@gmail.com>
2016-02-21 13:06:18 +0700

    connect v2

--- connect.php
+++ connect.php
@@ -5,8 +5,8 @@ define('DB_USER','root');
 define('DB_PASSWORD','');
 $conn=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: ".mtsql_error());
 $db=mysql_select_db(DB_NAME,$conn);
-$username=$_POST['user'];
-$password=$_POST['password'];
+/* $username=$_POST['username'];
+$password=$_POST['password']; */
 session_start();
 if(isset($_POST['submit'])) {
 	$query=mysql_query("SELECT *  FROM UserName where userName = '$_POST[username]' AND pass = '$_POST[password]'") or die(mysql_error());
